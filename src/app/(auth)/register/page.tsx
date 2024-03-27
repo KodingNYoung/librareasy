054e1469483ce9ca2b24f285d1b9160750a440c5
KodingNYoung <abiodunadebambo44@gmail.com>
2024-03-27 02:41:09 +0100

    fix build issue

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,13 @@
+import { PageFC } from "@/utilities/types";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Register",
+};
+
+const RegisterPage: PageFC = () => {
+  return <div>RegisterPage</div>;
+};
+
+export default RegisterPage;
---
+++ src/app/public/page.tsx
@@ -0,0 +1,13 @@
+import { PageFC } from "@/utilities/types";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Get files and materials",
+};
+
+const PublicPage: PageFC = () => {
+  return <div>PublicPage</div>;
+};
+
+export default PublicPage;
