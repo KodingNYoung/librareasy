cb17a75a8e247c7c52edfef1455217bf46e6dedc
KodingNYoung <abiodunadebambo44@gmail.com>
2024-08-19 21:26:32 +0100

    fix suspense boundary issue

--- src/app/(organization)/layout.tsx
+++ src/app/(organization)/layout.tsx
@@ -1,12 +1,14 @@
 import OrganizationLayout from "@/components/views/layouts/Organization";
 import { LayoutFC } from "@/utilities/types";
-import React from "react";
+import React, { Suspense } from "react";
 import Providers from "./providers";
 
 const Layout: LayoutFC = async ({ children }) => {
   return (
     <Providers>
-      <OrganizationLayout>{children}</OrganizationLayout>
+      <Suspense>
+        <OrganizationLayout>{children}</OrganizationLayout>
+      </Suspense>
     </Providers>
   );
 };
